a16a91eeec4f4637d80b40c09afbdd18a3298f5c
Maciej Bogdanski <m.bogdanskii@gmail.com>
2018-10-17 17:36:40 +0200

    add style for nav

--- app/script/script.js
+++ app/script/script.js
@@ -1,4 +1,3 @@
-
 // function for dashboard
 const analyse = document.querySelector("#analyse");
 const swich = document.querySelector('#swich');
@@ -7,45 +6,51 @@ const payment = document.querySelector('#payment');
 swich.addEventListener("click", function (event) {
     const target = event.target;
     analyse.classList.toggle('hidden');
-    analyse.classList.toggle('analyse-container')
+    analyse.classList.toggle('analyse-container');
     console.log(target, analyse);
+}
 
-});
-
+);
 bank.addEventListener("click", function () {
     window.location.assign("./dashboard.html")
-});
+}
 
-
+);
 payment.addEventListener("click", function () {
     window.location.assign("./transfer.html")
-})
+}
 
-
-// function for navigation
+) // function for navigation
 const logout = document.querySelector("#logout");
 logout.addEventListener('click', function () {
     window.location.assign("./index.html")
-});
+}
+
+);
 const search = document.querySelector("#search");
-const searchConatiner = document.querySelector('.search-conatiner')
+const searchConatiner = document.querySelector('.search-conatiner');
 search.addEventListener("click", function () {
-    searchConatiner.classList.toggle('active')
+    const faSearch = document.querySelector('.fa-search');
+    faSearch.style.color = "#4f8c9e";
+    searchConatiner.classList.toggle('active');
     searchConatiner.classList.toggle('hidden')
+}
+
+);
+const hamburger = document.querySelector("#hamburger");
+hamburger.addEventListener('click', function () {
+
+    const cntUsers = document.querySelectorAll('#cntUser');
+    for (const cntUser of cntUsers) {
+
+        cntUser.classList.toggle('active');
 
 
-});
 
 
 
+    }
 
+}
 
-
-const hamburger = document.querySelector("#hamburger")
-hamburger.addEventListener('click', function (e) {
-    console.log(e.target)
-    const cntUser = document.querySelector('#cntUser');
-    cntUser.style.display = "block";
-})
-
-
+)
